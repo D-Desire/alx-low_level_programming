559d44348ae80683f1761de8ce9975a06224776f
D-Desire <D-Desire>
2022-10-13 13:21:00 -0700

    My new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ *main- Serves as the entry point for the program
+ *
+ * Return: By default returns zero if no errors otherwise returns non-zero value
+ */
+int main(void)
 {
-	printf ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
