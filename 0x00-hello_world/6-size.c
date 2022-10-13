e7ba37a2ce5a4dadd129e50422e8078454064724
D-Desire <D-Desire>
2022-10-13 14:30:05 -0700

    My commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- *main -is the entry point of the program
- *
- *Return:zero if no error, Non-zero value if errors
-*/
+ *  * main - This is a programm
+ *   * Description: prints the sieze of variable types
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	printf("Size of char: %ld bytes(s)\n", sizeof(char));
-	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
