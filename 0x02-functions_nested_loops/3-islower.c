147b1ae67cffc3a7362d50368ca6c043d7b834e2
D-Desire <D-Desire>
2022-10-18 15:48:47 -0700

    My latest commmit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 nclude "main.h"
 /**
  * _islower - Checks if a character is lowercase
- * @c: The character to be checked 
+ * @c: The character to be checked
  *
  * Return: 1 if character is lowercase, 0 otherwise
 */
@@ -9,6 +9,6 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else 
+	else
 		return (0);
 }
